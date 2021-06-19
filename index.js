38f91cc763fe6d552b4fc22b29f62dd6b5922aac
Resbi-Anik <resbianik13@gmail.com>
2021-06-19 16:26:48 +0600

    create a simple server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Rest api');
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("rest api");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.listen(3000, () => {
+  console.log("listening at port 3000");
+});
