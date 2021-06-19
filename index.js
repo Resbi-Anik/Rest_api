bf36b68f4cb290aee9154d73d33239087c45d91e
Resbi-Anik <resbianik13@gmail.com>
2021-06-19 23:37:40 +0600

    urlencoded & static middleware

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Joi = require("joi");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 app.use(logger);
 
