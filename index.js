1801f380324d3f973f5e978976e55d2f7b384a94
Resbi-Anik <resbianik13@gmail.com>
2021-06-19 16:44:34 +0600

    set PORT for cross platform like dev & production server

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => {
-  console.log("listening at port 3000");
+const port = process.env.PORT || 3000
+console.log('port', port);
+
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
 });
