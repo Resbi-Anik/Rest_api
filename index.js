63852edbf5aea36bdc85866a9e1dc2a7e838faad
Resbi-Anik <resbianik13@gmail.com>
2021-06-19 16:50:43 +0600

    req.param obj and req.query obj

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-const port = process.env.PORT || 3000
-console.log('port', port);
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params);
+});
+
+app.get("/api/post/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+console.log("port", port);
 
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
