86bb96f72454066706182f072c9a3f4219e24075
Resbi-Anik <resbianik13@gmail.com>
2021-06-19 17:02:36 +0600

    status code 404 & parseInt for typecasting string to integer of request obj values

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+];
+
 app.get("/", (req, res) => {
   res.send("rest api");
 });
@@ -11,12 +16,18 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params);
+  // console.log(typeof(req.params.id));
+  const course = courses.find((value) => value.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("not available");
+  } else {
+    res.send(course);
+  }
 });
 
-app.get("/api/post/:year/:month", (req, res) => {
-  res.send(req.query);
-});
+// app.get("/api/post/:year/:month", (req, res) => {
+//   res.send(req.query);
+// });
 
 const port = process.env.PORT || 3000;
 console.log("port", port);
