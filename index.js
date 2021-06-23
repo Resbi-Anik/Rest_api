d88fa0482a424a82978bec789df7bc1813725d99
Resbi-Anik <resbianik13@gmail.com>
2021-06-24 00:02:29 +0600

    promise over callback .then .catch

--- index.js
+++ index.js
@@ -17,17 +17,21 @@
 // console.log('user value is:', user);
 // console.log('after');
 
-// using callback
+// using promise
 
-const getUser = (value, callback) =>{
-  console.log('reading a user');
-  setTimeout(()=>{
-   callback({user_value: value});
-  },2000)
+const getUser = (value) =>{
+  return new Promise((resolve, reject)=>{
+    console.log('reading a user');
+      setTimeout(()=>{
+      resolve({user_value: value});
+      },2000)
+  })
 }
 
 console.log('before');
-const user=getUser(1,(user)=>{
-    console.log('user value is:', user);
-})
+const user=getUser(1)
+.then(value=>console.log('user value is:', value))
 console.log('after');
+
+const err_values= Promise.reject(new Error('error to get value'))
+err_values.catch(err=> console.log(err))
